dd1b6599d12033a47ca09eb15090cc20174b2c05
Maxence KAMIONKA <maxence.kamionka@efrei.net>
2023-09-20 22:20:03 +0200

    Added a routes.js file for the DynamicEndpoin route Handler

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
 import express from 'express'
 import { stringify } from 'qs';
 //const express = require('express');  //doesn't work
 const app = express();
+import { DEHandler } from './routes.js';
 
 app.use(express.static('public'));
 
@@ -10,18 +10,6 @@ app.get('/', (request, response) => {
     response.sendFile('index.html');
 })
 
-app.get('/api/data/:number', (request, response) => {
-    const number = request.params.number;
-    const res = generateResponse(number);
-    response.json(res);
-});
-
-//returns the string as it is if not a number and multiply by 2 if it's one
-function generateResponse(number) {
-    if (isNaN(number)) {
-        return { result: number };
-    }
-    return { result: number * 2 };
-}
+app.get('/api/data/:number', DEHandler);
 
 app.listen('3000');
