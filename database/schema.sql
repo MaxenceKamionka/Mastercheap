f68ba9db08e34d0693984d833a2203cbebeaf9e7
Maxence KAMIONKA <maxence.kamionka@efrei.net>
2023-09-30 20:17:11 +0200

    Updated schema.sql and seed.sql to have serial id

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users(
-   id_user SMALLINT,
+   id_user SERIAL,
    user_name VARCHAR(20) NOT NULL,
    user_password VARCHAR(15) NOT NULL,
    user_email VARCHAR(50) NOT NULL,
@@ -10,8 +10,8 @@ CREATE TABLE users(
 );
 
 CREATE TABLE product(
-   id_product SMALLINT,
-	prod_name VARCHAR(30) NOT NULL,
+   id_product SERIAL,
+   prod_name VARCHAR(30) NOT NULL,
    prod_price DECIMAL(15,2) NOT NULL,
    prod_price_unit DECIMAL(15,2),
    prod_unit VARCHAR(5),
@@ -20,7 +20,7 @@ CREATE TABLE product(
 );
 
 CREATE TABLE tag(
-   id_tag SMALLINT,
+   id_tag SERIAL,
    tag_name VARCHAR(20) NOT NULL,
    tag_description VARCHAR(100),
    tag_img VARCHAR(100),
@@ -28,26 +28,26 @@ CREATE TABLE tag(
 );
 
 CREATE TABLE shopping_list(
-   id_shopping_list SMALLINT,
+   id_shopping_list SERIAL,
    shopping_list_name VARCHAR(50) NOT NULL,
    shopping_list_date DATE NOT NULL,
    shopping_list_desc VARCHAR(50),
-   id_user SMALLINT NOT NULL,
+   id_user INTEGER NOT NULL,
    PRIMARY KEY(id_shopping_list),
    FOREIGN KEY(id_user) REFERENCES users(id_user)
 );
 
 CREATE TABLE CORRESPOND(
-   id_product SMALLINT,
-   id_tag SMALLINT,
+   id_product INTEGER,
+   id_tag INTEGER,
    PRIMARY KEY(id_product, id_tag),
    FOREIGN KEY(id_product) REFERENCES product(id_product),
    FOREIGN KEY(id_tag) REFERENCES tag(id_tag)
 );
 
 CREATE TABLE POSSESS(
-   id_product SMALLINT,
-   id_shopping_list SMALLINT,
+   id_product INTEGER,
+   id_shopping_list INTEGER,
    quantity SMALLINT NOT NULL,
    PRIMARY KEY(id_product, id_shopping_list),
    FOREIGN KEY(id_product) REFERENCES product(id_product),
@@ -55,12 +55,11 @@ CREATE TABLE POSSESS(
 );
 
 /*
-Logical data Model:
-users = (id_user SMALLINT, user_name VARCHAR(20), user_password VARCHAR(15), user_email VARCHAR(50), user_adress VARCHAR(100));
-product = (id_product SMALLINT, prod_price DECIMAL(15,2), prod_price_unit DECIMAL(15,2), prod_unit VARCHAR(5), prod_grocery_name VARCHAR(30));
-tag = (id_tag SMALLINT, tag_name VARCHAR(20), tag_description VARCHAR(100), tag_img VARCHAR(100));
-shopping_list = (id_shopping_list SMALLINT, shopping_list_name VARCHAR(50), shopping_list_date DATE, shopping_list_desc VARCHAR(50), #id_user);
+users = (id_user SERIAL, user_name VARCHAR(20), user_password VARCHAR(15), user_email VARCHAR(50), user_adress VARCHAR(100));
+product = (id_product SERIAL, prod_price DECIMAL(15,2), prod_price_unit DECIMAL(15,2), prod_unit VARCHAR(5), prod_grocery_name VARCHAR(30));
+tag = (id_tag SERIAL, tag_name VARCHAR(20), tag_description VARCHAR(100), tag_img VARCHAR(100));
+shopping_list = (id_shopping_list SERIAL, shopping_list_name VARCHAR(50), shopping_list_date DATE, shopping_list_desc VARCHAR(50), #id_user);
 CORRESPOND = (#id_product, #id_tag);
-POSSESS = (#id_product, #id_shopping_list, quantity SMALLINT);
+INCLUDE = (#id_product, #id_shopping_list, quantity SMALLINT);
 */
 
